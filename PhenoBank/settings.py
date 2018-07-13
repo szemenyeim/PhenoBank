72ee680225ab6ff12d209da94f10f6285f2fed1b
szemenyeim <szemenyeim@gmail.com>
2018-07-13 09:35:34 +0200

    Ready for depl

--- PhenoBank/settings.py
+++ PhenoBank/settings.py
@@ -29,7 +29,7 @@ except ImportError:
     from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 if not DEBUG:
