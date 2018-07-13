82c622ef3b248a9d9dbbdc3389f942ac6f3677fa
szemenyeim <szemenyeim@gmail.com>
2018-07-13 09:47:41 +0200

    Deployment

--- PhenoBank/settings.py
+++ PhenoBank/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,3 +160,8 @@ UPLOAD_URL = '/upload/'
 UPLOAD_ROOT = os.path.join(BASE_DIR, 'upload/')
 MEDIA_URL = '/CACHE/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'CACHE/')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
