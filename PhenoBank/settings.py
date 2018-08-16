fa654ed2105edcfbd2fa548aa256cfd651bcf3d3
szemenyeim <szemenyeim@gmail.com>
2018-08-16 12:43:59 +0200

    Allow localhost

--- PhenoBank/settings.py
+++ PhenoBank/settings.py
@@ -31,7 +31,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['152.66.240.18']
+ALLOWED_HOSTS = ['152.66.240.18','deeplearning.iit.bme.hu','127.0.0.1']
 if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
