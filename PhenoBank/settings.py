e1e7e5b637bd3bdec010f887115383a9f3f44073
szemenyeim <szemenyeim@gmail.com>
2018-08-18 13:29:32 +0200

    Redirect added Https removed

--- PhenoBank/settings.py
+++ PhenoBank/settings.py
@@ -31,15 +31,15 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['152.66.240.18','deeplearning.iit.bme.hu','127.0.0.1']
+ALLOWED_HOSTS = ['152.66.240.18','deeplearning.iit.bme.hu','phenbank.eu','127.0.0.1']
 if not DEBUG:
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    #SESSION_COOKIE_SECURE = True
+    #CSRF_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
-    SECURE_HSTS_SECONDS = 3600
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_SSL_REDIRECT = True
+    #SECURE_HSTS_SECONDS = 3600
+    #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    #SECURE_HSTS_PRELOAD = True
+    #SECURE_SSL_REDIRECT = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
 
